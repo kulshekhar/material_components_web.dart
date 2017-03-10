1773af5b4356bfb88077522597d8c3bfa05a9752
kulshekhar <kulshekhar@users.noreply.github.com>
2017-03-10 17:21:07 +0530

    add radio button

--- lib/src/interop.dart
+++ lib/src/interop.dart
@@ -11,8 +11,26 @@ class mdc {
 
 @JS('mdc.textfield.MDCTextfield')
 class MDCTextfield {
+  external static MDCTextfield attachTo(Element root);
   external MDCTextfield(Element root);
   external destroy();
+  external initialSyncWithDom();
   external InputElement get input_;
   external get label_;
+  external bool get disabled;
+  external set disabled(bool disabled);
+}
+
+@JS('mdc.radio.MDCRadio')
+class MDCRadio {
+  external static MDCRadio attachTo(Element root);
+  external MDCRadio(Element root);
+  external destroy();
+  external initRipple_();
+  external InputElement get nativeControl_;
+  external get ripple_;
+  external bool get checked;
+  external set checked(bool checked);
+  external bool get disabled;
+  external set disabled(bool disabled);
 }
